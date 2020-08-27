c65dc26c6fa45f7c22f2f3cc889af82d8fab611b
Douglas Toledo da Silva <douglas22toledo@gmail.com>
2020-08-26 22:14:08 -0300

    done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+type CreateTransactionRequest = {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+};
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({
+    title,
+    value,
+    type,
+  }: CreateTransactionRequest): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total - value < 0)
+      throw Error('Not enough cash');
+
+    return this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
   }
 }
 
